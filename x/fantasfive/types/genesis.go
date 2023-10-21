05c8e75f188b156536935246124ff66c2722408c
rtmtree <bank23232525@gmail.com>
2023-10-21 15:54:09 +0700

    feat: init default MW and Team ids

--- x/fantasfive/types/genesis.go
+++ x/fantasfive/types/genesis.go
@@ -10,7 +10,12 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		SystemInfoList: []SystemInfo{},
+		SystemInfoList: []SystemInfo{
+			{
+				NextMWId:   DefaultIndex,
+				NextTeamId: DefaultIndex,
+			},
+		},
 		StoredMWList:   []StoredMW{},
 		StoredTeamList: []StoredTeam{},
 		// this line is used by starport scaffolding # genesis/types/default
